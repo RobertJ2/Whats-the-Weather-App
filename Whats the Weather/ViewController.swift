bd2027c2ff910a170365fc82c1179cce93fd1cfe
Robert Jackson <robertjacksonjr@cpe-2607-fcc8-ec4c-5d00-481-4f0a-de37-146d.dyn6.twc.com>
2017-12-04 18:53:59 -0500

    Initial Commit

---
+++ Whats the Weather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Whats the Weather
+//
+//  Created by Robert Jackson Jr on 12/4/17.
+//  Copyright © 2017 Robert Jackson Jr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
